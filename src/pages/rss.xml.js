ca4f1da2e71d8df7502038cb74ab66549a0ab82e
Buhlebethu Mkhonta <percymkhonta@gmail.com>
2025-08-20 18:45:34 +0200

    sorted delpoyment issue

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,16 @@
 import rss from '@astrojs/rss';
 
 export async function GET(context) {
-  const posts = await Astro.glob('../pages/posts/*.md');
+  const posts = await import.meta.glob('./posts/*.md');
+  const allPosts = await Promise.all(
+    Object.values(posts).map(post => post())
+  );
+
   return rss({
     title: 'Emaswati Skate | Blog',
     description: 'Latest updates from Emaswati Skate',
     site: context.site,
-    items: posts.map((post) => ({
+    items: allPosts.map((post) => ({
       title: post.frontmatter.title,
       pubDate: new Date(post.frontmatter.pubDate),
       description: post.frontmatter.description,
